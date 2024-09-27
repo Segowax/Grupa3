1f3192db228c998f7335da6d740fd4e35c781152
vanbasx <ivan.bas.vb@gmail.com>
2024-09-27 11:59:48 +0200

    Add Class Employer

---
+++ WebApp/WebApp.Database/Entietis/Employer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Database.Entietis
+{
+    internal class Employer
+    {
+        private int id;
+        private string userName;
+        private string password;
+        private string email;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+}

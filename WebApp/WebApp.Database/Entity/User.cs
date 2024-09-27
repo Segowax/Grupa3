a2fe30c0bac5f4722aa929c690b0c873218855f5
mrbeiser <g.danylko2007@gmail.com>
2024-09-27 11:55:58 +0200

    user object

---
+++ WebApp/WebApp.Database/Entity/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Database.Entity
+{
+    internal class User
+    {
+        private int Id { get; set; }
+        private string Nickname { get; set; }
+        private string Email { get; set; }
+        private string Password { get; set; }
+        private string DateCreated { get; set; }
+
+        public User(int id, string nickname, string email, string password, string dateCreated)
+        {
+            Id = id;
+            Nickname = nickname;
+            Email = email;
+            Password = password;
+            DateCreated = dateCreated;
+        }
+
+        public User() { }
+    }
+}

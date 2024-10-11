1a55c956ec40d3ebf7e0a8aecf54e136564ca479
oleh <oleh.olinyk.oo@gmail.com>
2024-10-11 12:14:05 +0200

    ConsoleApp

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var a = new User();
+        var b = new User();
+
+        a.Name = "Jacek";
+        a.Password = "password21321";
+
+        b.Name = "Ania";
+        b.Password = "passwordAni";
+
+        //a = b;
+
+        a.Name = "Ania";
+        b.Name = "Jacek";
+
+        a.Password = "paswordAni";
+        b.Password = "passwor1212d";
+
+        a = new User()
+        {
+            Name = b.Name,
+            Password = a.Password,
+        };
+        
+
+        Console.WriteLine($"A Name - {a.Name} Password - {a.Password}");
+        Console.WriteLine($"B Name - {b.Name} password - {b.Password}");
+       
+    }
+    internal class User
+    {
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+    }
+
+}

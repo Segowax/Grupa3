c55977bc8325870137ab7509116653695df71f43
mrbeiser <g.danylko2007@gmail.com>
2024-10-11 12:10:08 +0200

    Conole app

---
+++ WebApp/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        User daniel = new User();
+        User juzer = new User();
+
+        daniel.Name = "Daniel";
+        daniel.Surname = "Harmasz";
+
+        juzer.Name = "bombim";
+        juzer.Surname = "bimbom/";
+
+        Console.WriteLine($"old juzer \nName: {juzer.Name} Surname: {juzer.Surname}");
+
+        juzer = new User()
+        {
+            Name = daniel.Name,
+            Surname = daniel.Surname,
+        };
+
+        Console.WriteLine($"Name: {juzer.Name} Surname: {juzer.Surname}");
+        Console.WriteLine($"new juzer \nName: {juzer.Name} Surname: {juzer.Surname}");
+    }
+}
+
+public class User
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+}

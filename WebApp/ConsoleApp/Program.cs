1536b6d52f7c8bd66a20f04373cb6a78ed2674cd
Uczen <HXSXKU@gmail.com>
2024-10-11 12:20:03 +0200

    New console app added

---
+++ WebApp/ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    public static void Main(String[] args)
+    {
+        var a = new User();
+        var b = new User();
+
+        a.Name = "Jacek";
+        
+        b.Name = "Ania";
+
+        b.Password = a.Password;
+
+        a = new User()
+        {  
+            Password = b.Password
+        };
+
+        Console.WriteLine($"Name {a.Name} Password: {a.Password}");
+        Console.WriteLine($"Name {b.Name} Pasword: {b.Password}");
+
+
+
+    }
+    internal class User
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = Random.Shared.Next(1000, 10000).ToString();
+    }
+}

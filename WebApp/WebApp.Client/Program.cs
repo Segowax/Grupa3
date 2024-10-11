c1cdb50356d95c858fbbc749cc02bd6359abf17f
vanbasx <ivan.bas.vb@gmail.com>
2024-10-11 10:11:20 +0200

    change base adress uri

--- WebApp/WebApp.Client/Program.cs
+++ WebApp/WebApp.Client/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7238/") });
 
 await builder.Build().RunAsync();
 // {vanbasx} - rozumiem

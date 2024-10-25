3ad53b7f0ee786a507fb7bda2a68b9a69287011c
vanbasx <ivan.bas.vb@gmail.com>
2024-10-25 12:16:25 +0200

    create architekture

---
+++ WebApp/WebApp.Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WebApp.Domain.Role;
+
+namespace WebApp.Domain.Entities
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public Roles.RoleType Role { get; set; } = Roles.RoleType.User;
+        public User(int id, string name, string email, string password, Roles.RoleType role = Roles.RoleType.User)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            Role = role;
+        }
+    }
+}

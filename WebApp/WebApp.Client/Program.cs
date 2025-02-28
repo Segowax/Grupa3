751479299d667a6ee7bee93e81834bddf16df819
mrbeiser <g.danylko2007@gmail.com>
2025-02-28 12:11:36 +0100

    Sensory request

--- WebApp/WebApp.Client/Program.cs
+++ WebApp/WebApp.Client/Program.cs
@@ -6,10 +6,18 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+string? httpClientName = "sensory";
+ArgumentException.ThrowIfNullOrEmpty(httpClientName);
 
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+builder.Services.AddHttpClient(httpClientName, client =>
+{
+    client.BaseAddress = new Uri("https://my-wonderful-api-acgvdvaaa5d4b9ez.northeurope-01.azurewebsites.net");
+    client.DefaultRequestHeaders.UserAgent.ParseAdd("dotnet-docs");
+
 });
 
 await builder.Build().RunAsync();
